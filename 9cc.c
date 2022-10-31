19bd2c912b25c5017f2443ffd991983bc316a3e1
wadeus <wadeus.gar@gmail.com>
2022-10-31 16:55:39 +0900

    add addition and subtraction

--- 9cc.c
+++ 9cc.c
@@ -3,14 +3,33 @@
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "Incorrect number of arguments\n");
+        fprintf(stderr, "incorrect number of arguments\n");
         return 1;
     }
+    
+    char *p = argv[1];
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
+    printf("    mov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("    add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "unexpected letter: '%c'\n", *p);
+        return 1;
+    }
     printf("    ret\n");
     return 0;
 }
